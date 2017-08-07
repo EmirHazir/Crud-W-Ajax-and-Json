f90911204d0aafad54a87d4c7e5200fc11bbb33d
BurcuAla <BurcuAla@BURCUALA>
2017-08-07 15:32:45 +0300

    Add project files.

---
+++ MvcOperationWithAjaxAndJson/Controllers/ProductController.cs
@@ -0,0 +1,82 @@
+﻿using MvcOperationWithAjaxAndJson.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOperationWithAjaxAndJson.Controllers
+{
+    public class ProductController : Controller
+    {
+        ApplicationDbContext _dbContext;
+
+        public ProductController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // GET: Product
+        public ActionResult GetProducts()
+        {
+            var p = _dbContext.Products.ToList();
+
+            return Json(p, JsonRequestBehavior.AllowGet);
+        }
+
+        // GET: Product by id
+        public ActionResult Get(int id)
+        {
+            var p = _dbContext.Products.ToList().Find(x => x.Id == id);
+
+            return Json(p,JsonRequestBehavior.AllowGet);
+        }
+
+        // Post: Create product
+        [HttpPost]
+        public ActionResult Create([Bind(Exclude = "Id")]Product model)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Products.Add(model);
+                _dbContext.SaveChanges();
+
+            }
+            return Json(model,JsonRequestBehavior.AllowGet);
+        }
+
+        // Post: Update product
+        [HttpPost]
+        public ActionResult Update(Product model)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Entry(model).State = EntityState.Modified;
+                _dbContext.SaveChanges();
+            }
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
+
+        // Post: Delete product by id
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var p = _dbContext.Products.ToList().Find(x => x.Id == id);
+            if (p != null)
+            {
+                _dbContext.Products.Remove(p);
+                _dbContext.SaveChanges();
+            }
+            return Json(p,JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ MvcOperationWithAjaxAndJson/Scripts/Products.js
@@ -0,0 +1,143 @@
+﻿/// <reference path="jquery-1.10.2.js" />
+
+
+$(document).ready(function () {
+    getProducts();
+});
+
+//insert ve update check etmek icin bir variable oluştur ilk degeri false
+var isUpdateable = false;
+
+//Get products metodunu yaz ki bu sayfaya ilk yüklenecek veri
+
+function getProducts() {
+    $.ajax({
+        url: '/Product/GetProducts/',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            var rows = '';
+            $.each(data, function (i, item) {
+                rows += "<tr>"
+                rows += "<td>" + item.Id + "</td>"
+                rows += "<td>" + item.Name + "</td>"
+                rows += "<td>" + item.Price + "</td>"
+                rows += "<td> <button type='button' id='btnEdit' class='btn btn-xs btn-default' onclick='return getProductById(" + item.Id + ")'>Edit</button> <button type='button' id='btnDelete' class='btn btn-xs btn-danger' onclick='return deleteProductById(" + item.Id + ")'>Delete</button></td> </td>"
+                rows += "</tr>";
+                $("#listProducts tbody").html(rows);
+            });
+        },
+        error: function (err) {
+            alert("Error: " + err.responseText);
+        }
+    })
+}
+
+
+
+// Get product by id
+function getProductById(id) {
+    $("#title").text("Product Detail");
+    $.ajax({
+        url: '/Product/Get/' + id,
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            $("#Id").val(data.Id);
+            $("#Name").val(data.Name);
+            $("#Price").val(data.Price);
+            isUpdateable = true;
+            $("#productModal").modal('show');
+        },
+        error: function (err) {
+            alert("Error: " + err.responseText);
+        }
+    });
+}
+
+
+// Insert/ Update a product
+$("#btnSave").click(function (e) {
+
+    var data = {
+        Id: $("#Id").val(),
+        Name: $("#Name").val(),
+        Price: $("#Price").val()
+    }
+
+    if (!isUpdateable) {
+        $.ajax({
+            url: '/Product/Create/',
+            type: 'POST',
+            dataType: 'json',
+            data: data,
+            success: function (data) {
+                getProducts();
+                $("#productModal").modal('hide');
+                clear();
+            },
+            error: function (err) {
+                alert("Error: " + err.responseText);
+            }
+        })
+    }
+    else {
+        $.ajax({
+            url: '/Product/Update/',
+            type: 'POST',
+            dataType: 'json',
+            data: data,
+            success: function (data) {
+                getProducts();
+                isUpdateable = false;
+                $("#productModal").modal('hide');
+                clear();
+            },
+            error: function (err) {
+                alert("Error: " + err.responseText);
+            }
+        })
+    }
+});
+
+
+
+// Delete product by id
+function deleteProductById(id) {
+    $("#confirmModal #title").text("Delete Product");
+    $("#confirmModal").modal('show');
+    $("#confirmModal #btnOk").click(function (e) {
+        $.ajax({
+            url: "/Product/Delete/" + id,
+            type: "POST",
+            dataType: 'json',
+            success: function (data) {
+                getProducts();
+                $("#confirmModal").modal('hide');
+            },
+            error: function (err) {
+                alert("Error: " + err.responseText);
+            }
+        });
+
+        e.preventDefault();
+    });
+}
+
+
+// Set title for create new
+$("#btnCreate").click(function () {
+    $("#title").text("Create New");
+})
+
+// Close modal
+$("#btnClose").click(function () {
+    clear();
+});
+
+// Clear all items
+function clear() {
+    $("#Id").val("");
+    $("#Name").val("");
+    $("#Price").val("");
+}
